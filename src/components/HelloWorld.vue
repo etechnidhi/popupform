8c7016d0e6f093fd29e65e98bb71b04c4e1a894b
etechnidhi <nidhi_sharma@excellencetechnologies.in>
2018-07-19 12:12:29 +0530

    form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,17 +7,17 @@
       <b-modal id="modalPrevent" ref="modal" title="Submit your Detail" @ok="handleOk">
         <form @submit.stop.prevent="handleSubmit">
           <!-- <div class="form-group" :class="{'has-error': errors.has('name') }"> -->
-            <b-form-input class="input"  type="text" placeholder="Name" v-model="user.name"></b-form-input>
-            <b-alert variant="danger" show v-if="nameError && submitted" >{{error_name}}</b-alert>
+          <b-form-input class="input" type="text" placeholder="Name" v-model="user.name"></b-form-input>
+          <b-alert variant="danger" show v-if="!user.name && submitted">{{error_name}}</b-alert>
           <!-- </div> -->
-          <b-form-input class="input" type="email"  placeholder="Enter your Email" v-model="user.email"></b-form-input>
-          <b-alert variant="danger"  show v-if="emailError && submitted" >{{error_message}}</b-alert>
+          <b-form-input class="input" type="email" placeholder="Enter your Email" v-model="user.email"></b-form-input>
+          <b-alert variant="danger" show v-if="!user.email && submitted">{{error_message}}</b-alert>
           <b-form-input class="input" type="date" placeholder="Enter your DOB" icon="calendar-today" v-model="user.dob"></b-form-input>
-          <b-alert variant="danger" show v-if="dobError && submitted">{{error_message}}</b-alert>
+          <b-alert variant="danger" show v-if="!user.dob && submitted">{{error_dob}}</b-alert>
           <b-form-input class="input" type="password" placeholder="Enter your Password" v-model="user.password"></b-form-input>
-          <b-alert variant="danger" show v-if="passError && submitted">{{error_message}}</b-alert>
+          <b-alert variant="danger" show v-if="!user.password && submitted">{{error_pass}}</b-alert>
           <b-form-input class="input" type="password" placeholder="Enter your Confirm-Password" v-model="user.con_password"></b-form-input>
-          <b-alert variant="danger" show v-if="con_passError && submitted">{{error_message}}</b-alert>
+          <b-alert variant="danger" show v-if="!user.con_password && submitted">{{error_conpass}}</b-alert>
           <br />
           <b-form-checkbox id="checkbox1" value="accepted" v-model="status" unchecked-value="not_accepted">
             I accept the terms and policy
@@ -28,7 +28,7 @@
     </div>
     <!--  Interface controls -->
     <b-row>
-     
+  
       <b-col md="6" class="my-1">
         <b-form-group horizontal label="Sort" class="mb-0">
           <b-input-group>
@@ -42,7 +42,7 @@
           </b-input-group>
         </b-form-group>
       </b-col>
-      
+  
       <b-col md="6" class="my-1">
         <b-form-group horizontal label="Per page" class="mb-0">
           <b-form-select :options="pageOptions" v-model="perPage" />
@@ -73,38 +73,17 @@
     EDIT
   </b-button>
 </template>
-
-<template slot="row-details" slot-scope="row">
-  <b-card>
-    <ul>
-      <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
-    </ul>
-  </b-card>
-</template>
     </b-table>
-
     <b-row>
       <b-col md="6" class="my-1">
         <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0" />
       </b-col>
-    </b-row>
-
-    <!-- Info modal -->
-    <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
-      <pre>{{ modalInfo.content }}</pre>
-    </b-modal>
-    
+    </b-row>    
   </b-container>
 </template>
 
 <script>
-import VeeValidate from 'vee-validate';
-
   const users = [];
-  VeeValidate.Validator.extend('passphrase', {
-    // getMessage: field => 'Sorry, wrong pass phrase.',
-    validate: value => value.toUpperCase() == 'Demogorgon'.toUpperCase()
-});
   
   export default {
     data() {
@@ -120,8 +99,7 @@ import VeeValidate from 'vee-validate';
         userCount: 0,
         submitted: false,
         status: false,
-        fields: [
-          {
+        fields: [{
             key: "name",
             label: "Name",
             sortable: true,
@@ -143,11 +121,8 @@ import VeeValidate from 'vee-validate';
             label: "Actions"
           }
         ],
-        nameError:false,
-        emailError:false,
-        dobError:false,
-        passError:false,
-        con_passError:false,
+        answer1: "please ennter the field",
+        error_message: "",
         currentPage: 1,
         perPage: 5,
         totalRows: users.length,
@@ -159,9 +134,18 @@ import VeeValidate from 'vee-validate';
         modalInfo: {
           title: "",
           content: ""
+        },
+        rules: {
+          email: [{
+            require: true,
+            // pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: "please put valid email",
+            trigger: "blur"
+          }]
         }
       };
     },
+  
     computed: {
       sortOptions() {
         // Create an options list from our fields
@@ -192,30 +176,26 @@ import VeeValidate from 'vee-validate';
         // Prevent modal from closing
         evt.preventDefault();
         this.submitted = true;
+        // let re =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  
         if (!this.user.email) {
-          this.emailError = true
-          this.error_message="please enter email";
-          return false;
+          this.error_message = "please enter email";
         }
+        // if(!re.test(String(this.user.email).toLowerCase())){
+        //   this.error_message ="please enter valid email";
+        //   // return false;
+        // }
         if (!this.user.name) {
-          this.nameError= true;
-          this.error_name ="please enter name";
-          return false;
+          this.error_name = "please enter name";
         }
         if (!this.user.dob) {
-          this.dobError= true;
-          this.error_message="please enter dob";
-          return false;
-          
+          this.error_dob = "please enter dob";
         }
         if (!this.user.password) {
-          this.passError=true;
-          this.error_message="please enter password";
-          return false;
+          this.error_pass = "please enter password";
         }
         if (!this.user.con_password) {
-          this.con_passError=true;
-          this.error_message="please enter confirm password";
+          this.error_conpass = "please enter confirm password";
           return false;
         }
         if (!this.status) {
@@ -225,11 +205,8 @@ import VeeValidate from 'vee-validate';
           return true;
         }
       },
+  
       handleSubmit() {
-        // this.$validator.validateAll();
-        // if (!this.errors.any()) {
-        //     this.submitForm()
-        // }
         if (this.isEdit) {
           for (var i = 0; i < this.users.length; i++) {
             if (this.users[i].id == this.user["id"]) {
@@ -237,8 +214,6 @@ import VeeValidate from 'vee-validate';
               this.isEdit = false;
             }
           }
-          // eslint-disable-next-line
-          
         } else {
           this.users.push({
             id: ++this.userCount,
@@ -250,7 +225,6 @@ import VeeValidate from 'vee-validate';
           });
         }
         this.$refs.modal.hide();
-        // alert("form submitted successfully");
         this.submitted = false;
         this.user = {
           email: "",
@@ -259,15 +233,11 @@ import VeeValidate from 'vee-validate';
           password: "",
           con_password: ""
         };
-                   // console.log("***",JSON.stringify(this.users));
-
       },
       deleteItem: function(user) {
         this.users.splice(this.users.indexOf(user), 1);
       },
       edit: function(user) {
-        // eslint-disable-next-line 
-        // console.log(user);
         this.user = user.item;
         this.isEdit = true;
       }
